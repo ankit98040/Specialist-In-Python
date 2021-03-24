5494d38b187b47c3a07687073dff70dc69354f92
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-03-24 08:29:09 +0530

    Update app.py

--- Flask/DatabaseApp/app.py
+++ Flask/DatabaseApp/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("DatabaseApp")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb/data.sqlite'
 
 #ORM = Object Relational Mapper
 db = SQLAlchemy(app)
+print(db)
