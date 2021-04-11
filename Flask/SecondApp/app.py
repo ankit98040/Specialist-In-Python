3a27d482496375f757ad15bac7dfb9b79dedf940
Pramanik, Ankit <pramanik.85849@gmail.com>
2021-04-11 15:55:43 +0530

    second app

---
+++ Flask/SecondApp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask("NewApp")
+
+@app.route('/form')
+def myform():
+	return render_template('basic.html')
+
+@app.route('/data', methods=['GET'])
+def mydata():
+	if request.method == 'GET':
+		data = request.args.get('x')
+
+	return data
+
+app.run(port=5555, debug=True)
